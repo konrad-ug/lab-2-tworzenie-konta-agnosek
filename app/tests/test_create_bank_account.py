8e0f857b34f629796f6b842e68745f8eda5bc2f4
anosek <agatanosek01@gmail.com>
2022-10-20 17:43:26 +0200

    Feature 1, 2, 3

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -3,11 +3,29 @@ import unittest
 from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
-
+    imie = "Dariusz"
+    nazwisko = "Januszewski"
+    pesel = "12345678901"
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
+        pierwsze_konto = Konto(self.imie, self.nazwisko, self.pesel)
+        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imię nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        self.assertNotEqual(pierwsze_konto.pesel, "Niepoprawny pesel!", "Niepoprawny pesel!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+    
+    def test_zbyt_dlugi_pesel(self):
+        pesel = "1234567890112123"
+        pierwsze_konto = Konto(self.imie, self.nazwisko, pesel)
+        self.assertEqual(pierwsze_konto.pesel, "Niepoprawny pesel!", "Niepoprawny pesel!")
+
+    def test_zbyt_krotki_pesel(self):
+        pesel = "12345"
+        pierwsze_konto = Konto(self.imie, self.nazwisko, pesel)
+        self.assertEqual(pierwsze_konto.pesel, "Niepoprawny pesel!", "Niepoprawny pesel!")
+
+    def test_kod_promo(self):
+        promo = "PROM_XYZ"
+        pierwsze_konto = Konto(self.imie, self.nazwisko, self.pesel, promo)
+        self.assertEqual(pierwsze_konto.promo, "PROM_XYZ", "Kod promo nie został zapisany!")
 
     #tutaj proszę dodawać nowe testy
